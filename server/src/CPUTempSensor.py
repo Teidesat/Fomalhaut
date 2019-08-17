e9c67af9fc922c02c6491000509cfd65fff2599d
AndresCalimero <andrescalimero@hotmail.es>
2019-08-17 01:02:56 +0100

    Improved exception handling

--- server/src/CPUTempSensor.py
+++ server/src/CPUTempSensor.py
@@ -4,10 +4,11 @@ from src.Sensor import Sensor
 class CPUTempSensor(Sensor):
 
     def __init__(self, sensor_id):
-        self.__temp_file = open('/sys/class/thermal/thermal_zone0/temp')
+        self.__temp_file = open('/sys/class/thermal/thermal_zone0/temp', 'r')
         super().__init__(sensor_id=sensor_id)
 
     def get_value(self):
+        self.__temp_file.seek(0)
         return float(self.__temp_file.read()) / 1000
 
     def get_type(self):
