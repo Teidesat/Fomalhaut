f0e1942c6312f9fdc61da01e65450d0dbe71c10d
Jorge Sierra <jsierra.siete@gmail.com>
2019-09-12 18:18:32 +0200

    Fixed sensor bug

--- python-server/src/sensors/SensorsProvider.py
+++ python-server/src/sensors/SensorsProvider.py
@@ -44,10 +44,12 @@ class SensorsProvider:
     def try_import_sensor_module(module_name, sensor_name, logger):
         ok = False
         try:
-            sensor = getattr(__import__(module_name), sensor_name)
+            module = __import__(module_name, fromlist=['object'])
+            sensor = getattr(module, sensor_name)
             logger.log('Loaded module for \'%s\'' % sensor_name, Logger.LogLevel.DEBUG)
             ok = True
-        except:
+        except Exception as e:
+            logger.log('Couldnt load module for \'%s\', %s' % (sensor_name, str(e)), Logger.LogLevel.DEBUG)
             logger.log('Couldnt load module for \'%s\' (is this Linux exclusive?)' % sensor_name, Logger.LogLevel.WARNING)
             sensor = None
         return ok, sensor
@@ -56,13 +58,13 @@ class SensorsProvider:
     def get_available_sensors(simulate=False, logger=None):
         sensors = []
         if not simulate:
+            DS18B20Sensor_ok, DS18B20Sensor   = SensorsProvider.try_import_sensor_module('src.sensors.DS18B20Sensor', 'DS18B20Sensor', logger)
             W1ThermSensor_ok, W1ThermSensor   = SensorsProvider.try_import_sensor_module('w1thermsensor', 'W1ThermSensor', logger)
-            DS18B20Sensor_ok, DS18B20Sensor   = SensorsProvider.try_import_sensor_module('src.DS18B20Sensor', 'DS18B20Sensor', logger)
-            CPUTempSensor_ok, CPUTempSensor   = SensorsProvider.try_import_sensor_module('src.CPUTempSensor', 'CPUTempSensor', logger)
-            MMA8451QSensor_ok, MMA8451QSensor = SensorsProvider.try_import_sensor_module('src.MMA8451QSensor', 'MMA8451QSensor', logger)
-            HMC5883LSensor_ok, HMC5883LSensor = SensorsProvider.try_import_sensor_module('src.HMC5883LSensor', 'HMC5883LSensor', logger)
-            BMP180Sensor_ok, BMP180Sensor     = SensorsProvider.try_import_sensor_module('src.BMP180Sensor', 'BMP180Sensor', logger)
-            DHT22Sensor_ok, DHT22Sensor       = SensorsProvider.try_import_sensor_module('src.DHT22Sensor', 'DHT22Sensor', logger)
+            CPUTempSensor_ok, CPUTempSensor   = SensorsProvider.try_import_sensor_module('src.sensors.CPUTempSensor', 'CPUTempSensor', logger)
+            MMA8451QSensor_ok, MMA8451QSensor = SensorsProvider.try_import_sensor_module('src.sensors.MMA8451QSensor', 'MMA8451QSensor', logger)
+            HMC5883LSensor_ok, HMC5883LSensor = SensorsProvider.try_import_sensor_module('src.sensors.HMC5883LSensor', 'HMC5883LSensor', logger)
+            BMP180Sensor_ok, BMP180Sensor     = SensorsProvider.try_import_sensor_module('src.sensors.BMP180Sensor', 'BMP180Sensor', logger)
+            DHT22Sensor_ok, DHT22Sensor       = SensorsProvider.try_import_sensor_module('src.sensors.DHT22Sensor', 'DHT22Sensor', logger)
 
             if W1ThermSensor_ok:
                 # All 1wire DS18B20 temperature sensors
