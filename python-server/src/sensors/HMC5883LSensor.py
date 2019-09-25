f400477ff5a563f45934f4e0ad2cd5c367615ebc
AndresCalimero <andrescalimero@hotmail.es>
2019-09-26 00:50:43 +0100

    Fixed HMC5883L sensor

--- python-server/src/sensors/HMC5883LSensor.py
+++ python-server/src/sensors/HMC5883LSensor.py
@@ -7,11 +7,8 @@ class HMC5883LSensor(I2CSensor):
         super().__init__(sensor_id=sensor_id, address=0x1E, value_reg=0x03, value_reg_size=6, config_reg=0x00)
 
     def configure_sensor(self, bus, address, config_reg):
-        # Configuration register A (01110000)
-        bus.write_byte_data(address, config_reg, 0x70)
-
-        # Configuration register B (00100000)
-        bus.write_byte_data(address, config_reg + 1, 0x20)
+        # Configuration register A
+        bus.write_byte_data(address, config_reg, 0x60)
 
         # Mode register (continuous)
         bus.write_byte_data(address, config_reg + 2, 0x00)
@@ -20,13 +17,19 @@ class HMC5883LSensor(I2CSensor):
         value = [0, 0, 0]
 
         # X axis
-        value[0] = super().cpl2(data[0] * 256 + data[1], 16)
-
-        # Y axis
-        value[1] = super().cpl2(data[2] * 256 + data[3], 16)
+        value[0] = data[0] * 256 + data[1]
+        if value[0] > 32767:
+            value[0] -= 65536
 
         # Z axis
-        value[2] = super().cpl2(data[4] * 256 + data[5], 16)
+        value[2] = data[2] * 256 + data[3]
+        if value[2] > 32767:
+            value[2] -= 65536
+
+        # Y axis
+        value[1] = data[4] * 256 + data[5]
+        if value[1] > 32767:
+            value[1] -= 65536
 
         return value
 
@@ -34,4 +37,4 @@ class HMC5883LSensor(I2CSensor):
         return 'compass'
 
     def get_unit(self):
-        return '°'
+        return 'gauss'
