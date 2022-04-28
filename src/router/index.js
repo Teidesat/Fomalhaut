0a628a13ae5d15138d626558902c234d1329bd54
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-04-28 10:21:50 +0100

    changed import statements for routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,36 +3,63 @@ import {
   createWebHistory,
   createWebHashHistory,
 } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import CommunicationsView from "../views/CommunicationsView";
-import LocationView from "../views/LocationView";
-import SensorsView from "../views/SensorsView";
-import TrackingView from "../views/TrackingView";
-import EncoderView from "../views/EncoderView";
-import DecoderView from "../views/DecoderView";
-import ConfigurationView from "../views/ConfigurationView";
-import LogsView from "../views/LogsView";
-import AboutView from "../views/AboutView.vue";
 
 const routes = [
-  { path: "/", name: "home", component: HomeView },
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
+  },
   {
     path: "/communications",
     name: "communications",
-    component: CommunicationsView,
+    component: () => import("../views/CommunicationsView.vue"),
   },
-  { path: "/location", name: "location", component: LocationView },
-  { path: "/sensors", name: "sensors", component: SensorsView },
-  { path: "/tracking", name: "tracking", component: TrackingView },
-  { path: "/encoder", name: "encoder", component: EncoderView },
-  { path: "/decoder", name: "decoder", component: DecoderView },
+  {
+    path: "/location",
+    name: "location",
+    component: () => import("../views/LocationView.vue"),
+  },
+  {
+    path: "/sensors",
+    name: "sensors",
+    component: () => import("../views/SensorsView.vue"),
+  },
+  {
+    path: "/tracking",
+    name: "tracking",
+    component: () => import("../views/TrackingView.vue"),
+  },
+  {
+    path: "/encoder",
+    name: "encoder",
+    component: () => import("../views/EncoderView.vue"),
+  },
+  {
+    path: "/decoder",
+    name: "decoder",
+    component: () => import("../views/DecoderView.vue"),
+  },
+  {
+    path: "/temperaturemap",
+    name: "temperaturemap",
+    component: () => import("../views/TemperatureMapView.vue"),
+  },
   {
     path: "/configuration",
     name: "configuration",
-    component: ConfigurationView,
+    component: () => import("../views/ConfigurationView.vue"),
   },
-  { path: "/logs", name: "logs", component: LogsView },
-  { path: "/about", name: "about", component: AboutView },
+  {
+    path: "/logs",
+    name: "logs",
+    component: () => import("../views/LogsView.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
 ];
 
 const router = createRouter({
