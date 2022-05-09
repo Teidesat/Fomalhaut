8e253d9c8e1940b1da2e09f5e44afd9a3d631d16
FabioBianchiniCano <alu0101040929@ull.edu.es>
2022-05-09 09:08:46 +0100

    init about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,1 +1,45 @@
-<template>AboutView.vue</template>
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-8 text-start">
+        <hr />
+        <h1>About</h1>
+        <ul>
+          <h3>App made by:</h3>
+          <li>Jorge Sierra Acosta</li>
+          <li>Fabio Ovidio Bianchini Cano</li>
+        </ul>
+
+        <ul>
+          <h5>Technologies used:</h5>
+          <li>Vue Cli</li>
+          <li>Vue Router</li>
+          <li>Bootstrap</li>
+          <li>ThreeJS</li>
+          <li>Electron</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  padding-left: 1.3em;
+}
+li:before {
+  content: "\f105"; /* FontAwesome Unicode */
+  font-family: FontAwesome;
+  display: inline-block;
+  margin-left: -1.3em; /* same as padding-left set on li */
+  width: 1.3em; /* same as padding-left set on li */
+}
+</style>
