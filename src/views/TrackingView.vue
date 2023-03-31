48782f19b8b92d9a71faead8aaea1bf68746a136
Sergio Tabares <alu0101124896@ull.edu.es>
2023-03-31 22:38:00 +0100

    frame update under demand

--- src/views/TrackingView.vue
+++ src/views/TrackingView.vue
@@ -1,1 +1,37 @@
-<template>TrackingView.vue</template>
+<template>
+  <div>
+    <img
+      :src="imageUrl"
+      alt="Webcam Stream"
+      width="640"
+      height="480"
+      :key="imageUrl"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: "",
+    };
+  },
+  async created() {
+    try {
+      const streamUrl = "http://localhost:5000/stream";
+
+      setInterval(() => {
+        fetch(streamUrl, { method: "POST" })
+          .then((response) => response.blob())
+          .then((blob) => {
+            this.imageUrl = URL.createObjectURL(blob);
+          })
+          .catch((error) => console.error(error));
+      }, 50);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
