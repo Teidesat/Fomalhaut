1b6a7aee2f3e9206784f2956a786c9753f770bc6
mikelmujika16 <124912823+mikelmujika16@users.noreply.github.com>
2025-02-16 15:28:14 +0000

    change css format for panel divs #29

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,29 @@ import SidePanel from "./components/SidePanel/SidePanel";
 import About from "./components/pages/About/About";
 import TemperatureMap from "./components/pages/satellite/TemperatureMap/TemperatureMap";
 import Logs from "./components/pages/Logs/Logs";
+import Orbit from "./components/pages/satellite/Orbit/Orbit";
 
 function App() {
   return (
     <BrowserRouter>
-      <TopPanel />
-      <div className="App">
-        <SidePanel />
-        <div className="App-body">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/temperaturemap" element={<TemperatureMap />} />
-            <Route path="/logs" element={<Logs />} />
-          </Routes>
+      <div className="app-container">
+        <TopPanel />
+        <div className="content-wrapper">
+          <SidePanel />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/temperaturemap" element={<TemperatureMap />} />
+              <Route path="/location" element={<Orbit />} />
+              <Route path="/logs" element={<Logs />} />
+            </Routes>
+          </main>
         </div>
       </div>
     </BrowserRouter>
   );
 }
+
 
 export default App;
