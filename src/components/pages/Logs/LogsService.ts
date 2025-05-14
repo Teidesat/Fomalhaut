ca36a87716b541859f1e2d54d84a79ac9b01bd35
AlvaroRGZ <alvarorgten@gmail.com>
2025-05-14 12:02:44 +0100

    Add service promise subscribers in Logs #4

--- src/components/pages/Logs/LogsService.ts
+++ src/components/pages/Logs/LogsService.ts
@@ -3,20 +3,29 @@ import { Log } from "./Log.model";
 
 
 class LogsService {
-  private baseUrl: string;
 
-  constructor() {
-    this.baseUrl = 'http://localhost:8000/api/logvault/';
-  }
+  private baseUrl: string = 'http://localhost:8000/api/logvault/';
 
-  async getAllLogs(query: string = ''): AxiosResponse<Log[]> {
-    return axios.get(`${this.baseUrl}?${query}`, {
+  private baseHeader: any = {
       headers: {
         Authorization: 'Bearer holis123'
-      },
-    });
+      }
+    };
+
+  constructor() {}
+
+  getAllLogs(): Promise<AxiosResponse<Log[]>> {
+    return axios.get(`${this.baseUrl}all`, this.baseHeader);
   }
   
+  getLogsFilteredBy(query: string = ''): Promise<AxiosResponse<Log[]>> {
+    console.log("Calling " + `${this.baseUrl}?${query}`);
+    if (query.length === 0) {
+      return this.getAllLogs();
+    }
+    return axios.get(`${this.baseUrl}filter?${query}`, this.baseHeader);
+  }
+
   /*
   async getLogById(id: number): Promise<Log> {
     const response: AxiosResponse<Log> = await axios.get(`${this.baseUrl}${id}/`, {
