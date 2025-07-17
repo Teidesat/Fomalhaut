dd47b10321935d5c6d059bd247cf92c2a8c3ffd1
Sergio Tabares <alu0101124896@ull.edu.es>
2025-07-17 22:22:40 +0100

    Add GitHub Pages support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Fomalhaut",
   plugins: [react()],
   server: {
     host: true,
